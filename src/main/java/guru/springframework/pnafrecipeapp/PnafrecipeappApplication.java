ef7dcfec69014e244d9907cfbe488b6e40e54316
pankajnafria <inafsdevelope@gmail.com>
2021-09-24 22:51:57 -0700

    Initial Project

---
+++ src/main/java/guru/springframework/pnafrecipeapp/PnafrecipeappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.pnafrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnafrecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnafrecipeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/pnafrecipeapp/PnafrecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.pnafrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnafrecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
